f43e47d91516f7ce09513ce55c6373d7c876f37c
miketeow <27071053+miketeow@users.noreply.github.com>
2024-05-14 20:35:45 +0800

    Setup drizzle and neon database

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,12 +1,8 @@
-import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const DashboardPage = () => {
   return (
     <main>
-      <nav>
-        <UserButton afterSignOutUrl="/" />
-      </nav>
       <div>Dashboard Page</div>
     </main>
   );
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,11 +1,19 @@
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import { z } from "zod";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c) => {
+app.get("/hello", clerkMiddleware(), (c) => {
+  const auth = getAuth(c);
+
+  if (!auth?.userId) {
+    return c.json({ error: "Unauthorized" });
+  }
   return c.json({ message: "Hello World!" });
 });
 
