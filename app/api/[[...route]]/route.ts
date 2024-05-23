4fdfb1970e69f39594f6b04265541e5f8b6add82
miketeow <27071053+miketeow@users.noreply.github.com>
2024-05-23 20:50:49 +0800

    Add categories

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -4,6 +4,7 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import { z } from "zod";
 import accounts from "./accounts";
+import categories from "./categories";
 import { HTTPException } from "hono/http-exception";
 export const runtime = "edge";
 
@@ -16,7 +17,9 @@ app.onError((err, c) => {
   return c.json({ error: "Internal Error" });
 });
 
-const routes = app.route("/accounts", accounts);
+const routes = app
+  .route("/accounts", accounts)
+  .route("/categories", categories);
 
 export const GET = handle(app);
 export const POST = handle(app);
